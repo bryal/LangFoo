49a9be5b8ddaa0e750fff05be0d574d7b0a52b51
Johan Johansson <96.bryal@gmail.com>
2018-04-11 22:35:59 +0200

    Combine Done and Remains into Ok. Add binop/expression parsers

--- lex.hs
+++ lex.hs
@@ -19,33 +19,25 @@ data Lexer c = Ch c
 instance IsString (Lexer Char) where
   fromString = Str
 
-data Result c = Done [c]
-              | Remains [c] [c]
+data Result c = Ok [c] [c]
               | Err
   deriving (Show, Eq)
 
 maybeDone :: Result c -> Maybe [c]
-maybeDone (Done s) = Just s
-maybeDone _        = Nothing
-
-remToResult :: [c] -> [c] -> Result c
-remToResult s []  = Done s
-remToResult s rem = Remains s rem
+maybeDone (Ok s []) = Just s
+maybeDone _         = Nothing
 
 mapResult :: ([c] -> [d]) -> Result c -> Result d
-mapResult f (Done s)       = Done (f s)
-mapResult f (Remains s s') = remToResult (f s) (f s')
-mapResult f Err            = Err
+mapResult f (Ok s s') = Ok (f s) (f s')
+mapResult f Err       = Err
 
 mapMatched :: ([c] -> [c]) -> Result c -> Result c
-mapMatched f (Done s)       = Done (f s)
-mapMatched f (Remains s s') = Remains (f s) s'
-mapMatched f Err            = Err
+mapMatched f (Ok s s') = Ok (f s) s'
+mapMatched f Err       = Err
 
 bindRem :: ([c] -> Result c) -> Result c -> Result c
-bindRem mf (Done s)       = bindRem mf (Remains s [])
-bindRem mf (Remains s s') = mapMatched (s++) (mf s')
-bindRem mf Err            = Err
+bindRem mf (Ok s s') = mapMatched (s++) (mf s')
+bindRem mf Err       = Err
 
 toChar :: Int -> Char
 toChar = toEnum
@@ -61,15 +53,14 @@ to c1 c2 = Any (fmap (Ch . toChar) [fromEnum c1 .. fromEnum c2])
 
 lexChar :: (Eq c) => c -> [c] -> Result c
 lexChar c [] = Err
-lexChar c (c':[]) | c == c'   = Done (c:[])
+lexChar c (c':[]) | c == c'   = Ok (c:[]) []
                   | otherwise = Err
-lexChar c s | c == head s = Remains (take 1 s) (tail s)
+lexChar c s | c == head s = Ok (take 1 s) (tail s)
             | otherwise   = Err
 
 lexWild :: (Eq c) => [c] -> Result c
 lexWild []     = Err
-lexWild (c:[]) = Done (c:[])
-lexWild (c:s)  = Remains (c:[]) s
+lexWild (c:s)  = Ok (c:[]) s
 
 lexSeq' :: (Eq c) => [Lexer c] -> Result c -> Result c
 lexSeq' [] r          = r
@@ -77,25 +68,25 @@ lexSeq' ((:*) l:ls) r = bindRem (lexStarFollowedBy l (Seq ls)) r
 lexSeq' (l:ls) r      = lexSeq' ls (bindRem (lex l) r)
 
 lexSeq :: (Eq c) => [Lexer c] -> [c] -> Result c
-lexSeq l s = lexSeq' l (Remains [] s)
+lexSeq l s = lexSeq' l (Ok [] s)
 
 lexStar :: (Eq c) => Lexer c -> [c] -> Result c
 lexStar l s = case lex l s of
-  Done s'        -> Done s'
-  Remains s' s'' -> mapMatched (s'++) (lexStar l s'')
-  Err            -> if null s then Done [] else Remains [] s
+  Ok s' s'' -> mapMatched (s'++) (lexStar l s'')
+  Err       -> Ok [] s
 
 -- Note: Greedy
 lexStarFollowedBy :: (Eq c) => Lexer c -> Lexer c -> [c] -> Result c
 lexStarFollowedBy l l' s = lex (Seq [l, (l~*), l'] ~| l') s
 
 lex :: (Eq c) => Lexer c -> [c] -> Result c
-lex (Ch c) s     = lexChar c s
-lex Wildcard s   = lexWild s
-lex (Str s1) s2  = maybe Err (remToResult s1) (stripPrefix s1 s2)
-lex (Any ls) s   = fromMaybe Err (find (/= Err) (fmap (flip lex s) ls))
-lex (Seq ls) s   = lexSeq ls s
-lex ((:*) l) s   = lexStar l s
+lex (Ch c) s    = lexChar c s
+lex Wildcard s  = lexWild s
+lex (Str s1) s2 = maybe Err (Ok s1) (stripPrefix s1 s2)
+lex (Any ls) s  = fromMaybe Err (find (/= Err) (fmap (flip lex s) ls))
+lex (Seq (l:(Seq ls):ls')) s = lex (Seq (l:(ls++ls'))) s
+lex (Seq ls) s  = lexSeq ls s
+lex ((:*) l) s  = lexStar l s
 
 -- | No remainder allowed
 lexExact :: (Eq c) => Lexer c -> [c] -> Maybe [c]
@@ -103,9 +94,32 @@ lexExact l s = maybeDone (lex l s)
 
 --------------------------------------------------------
 
-digit = '0' `to` '9'
-int = (digit~+)
-float = Seq [int, ".", int]
+digit  = '0' `to` '9'
+int    = (digit~+)
+float  = int ~& "." ~& int
+number = float ~| int
+
 letter = ('a' `to` 'z') ~| ('A' `to` 'Z')
-str = Seq [Ch '"', (Wildcard~*), Ch '"']
-p
+ident  = letter ~& ((letter ~| digit ~| "_")~*)
+
+str    = Seq [Ch '"', (Wildcard~*), Ch '"']
+
+-- Whitespace
+wsChar = Any [" ", "\t", "\r", "\n"]
+ws     = (wsChar~+)
+wsOpt  = (wsChar~*)
+
+binop arg op = ((arg ~& wsOpt ~& op)~+) ~& wsOpt ~& arg
+
+factor     = number ~| Seq ["(", wsOpt, expr, wsOpt, ")"]
+mulArg     = factor
+mul        = binop mulArg "*"
+divArg     = mul ~| mulArg
+div'       = binop divArg "/"
+addArg     = div' ~| divArg
+add        = binop addArg "+"
+subArg     = add ~| addArg
+sub        = binop subArg "-"
+expr       = sub ~| subArg
+
+-- Operator precedence: *, /, +, -
